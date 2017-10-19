891bb67c5ea7715bc9a543f3756c9008b965655f
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-19 19:07:04 +0200

    add title

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -4,6 +4,9 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
+        <h1>
+          Armies at War
+        </h1>
         {this.props.children}
       </div>
     )
