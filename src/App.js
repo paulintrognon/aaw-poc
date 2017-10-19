e8439d92b1b211c5538283f4ba63034f7e8bad51
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-19 19:01:39 +0200

    add react part

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+
+import store from './store.js';
+
+import history from './services/history';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Layout>
+              <Route path="/" exact={true} component={Home}></Route>
+          </Layout>
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
